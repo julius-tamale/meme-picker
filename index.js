49a43470acd537740b3a75f02678c37a6c5fd0aa
julius-tamale <juliotamex@proton.me>
2023-04-25 20:26:58 +0200

    pumpkin meme update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { catsData } from "/data.js";
 
-const emotionRadios = document.getElementById('emotion-radios');
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', e => {
     const radioSelection = document.getElementsByClassName('radio')
@@ -10,6 +15,24 @@ emotionRadios.addEventListener('change', e => {
 
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 })
+
+getImageBtn.addEventListener('click', () => {
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML = `
+    <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="CAT ALT TEXT"
+    >`
+    memeModal.style.display = 'flex'
+})
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
 //create a function to iterate through catsData and return array of emotions
 function getEmotionsArray(cats) {
     const emotionsArray = []
@@ -23,6 +46,37 @@ function getEmotionsArray(cats) {
     }
 
     return emotionsArray
+}
+
+//create a function to sort emotions with GIFs
+function getMatchingCatsArray() {
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }
+
+        })
+        return matchingCatsArray
+    }  
+}
+
+function getSingleCatObject() {
+    const catsArray = getMatchingCatsArray()
+    
+    if (catsArray.length === 1){
+        return catsArray[0]
+    }
+    else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
 }
 
 
